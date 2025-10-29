c830b282ded97447edd74509970775f520607f33
CHORY-Chanrady <chorychanrady168@gmail.com>
2025-10-30 00:09:03 +0700

    third commit

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,3 @@
-import logo from "../assets/images/logo.png";
 import "../App.css";
 import { Link } from "react-router-dom";
 
@@ -9,7 +8,11 @@ function Hero() {
         <div className="max-w-6xl mx-auto flex items-center justify-between relative">
           {/* Left: Logo */}
           <div className="flex-shrink-0">
-            <img src={logo} alt="Logo" className="h-12 w-auto" />
+            <img
+              src="/public/images/logo.png"
+              alt="Logo"
+              className="h-12 w-auto"
+            />
           </div>
 
           {/* Center: Navigation */}
